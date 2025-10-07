ea2ffdcea34ee3aae885fbfe719def1780b416fb
Diones <pereiradiones987@gmail.com>
2025-10-07 11:34:04 -0300

    componente input

---
+++ frontend/src/components/auth/signin-form.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useState } from "react";
+import Input from "../ui/input";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+const SigninForm = () => {
+  const router = useRouter();
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  //Depois fazer a validação dos campos usando o zod
+
+  const handleEnterButton = async () => {
+    router.replace('/home');
+  }
+
+  return (
+    <>
+      <Input
+        placeholder='Digite seu email'
+        value={emailField}
+        onChange={t => setEmailField(t)}
+      />
+      <Input
+        placeholder='Digite sua senha'
+        value={passwordField}
+        onChange={t => setPasswordField(t)}
+        password
+      />
+
+      <button onClick={handleEnterButton}>Entrar</button>
+    </>
+  )
+}
+
+export default SigninForm
